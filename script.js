965547be55f9abd0e391e73974e6a0b38dfed313
DEEPESH-0911 <deepesh51172@gmail.com>
2024-12-13 15:29:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add interactive features if needed.
+document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thank you for reaching out! I'll get back to you soon.");
+  });
